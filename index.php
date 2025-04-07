71c9e66e62badb69a2727102c24ceb112e454079
Andres Guaqueta <andresguaquetagonzalez@gmail.com>
2025-04-07 14:53:55 -0500

    first upload

---
+++ index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TRANSPORTES ELITE S.A.S</title>
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <link rel="shortcut icon" href="imagenes/logoicon.ico" />
+    <link rel="stylesheet"
+    href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link
+    href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css"
+    rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;
+    0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,
+    900&family=Smooch+Sans:wght@100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header class="header">
+        <div class="menu container">
+            <nav class="navbar">
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="#servicios-elite">Servicios</a></li>
+                    <li><a href="quienes.php">Quienes Somos</a></li>
+                    <li><a href="contacto.php">Contacto</a></li>
+                </ul>
+            </nav>
+        </div>
+        
+        <div class="right-content">
+            <div id="menu-icon">☰</div>
+        </div>
+
+        <section class="header-bottom">
+            <div class="header-content container">
+                <h5 data-aos="fade-right" data-aos-duration="1400">¡Bienvenidos, somos...!</h5>
+                <h1 data-aos="zoom-in-left" data-aos-duration="1400" 
+                data-aos-delay="200">ELITE</h1>
+                <p data-aos="fade-right" data-aos-duration="1400" 
+                data-aos-delay="300">
+                Nos especializamos en el transporte o traslado de pasajeros, 
+                contamos con amplia experiencia en el sector garantizando un 
+                servicio seguro, eficiente y puntual.
+                </p>
+                <div class="boton" data-aos="flip-down" data-aos-duration="1400" 
+                data-aos-delay="400"><a href="#"class="btn-1">Información</a>
+                </div>
+            </div> 
+            
+            <div class="bottom-img"data-aos="zoom-in-left" data-aos-duration="1400">
+                <img src="imagenes/bus.png">
+            </div>
+        </section>
+        
+        <div class="icons">
+            <a href="#" data-aos="fade-in" data-aos-duration="1400" 
+            data-aos-delay="600"><i id="ico1" class="ri-facebook-box-fill"></i></a>
+            <a href="#" data-aos="fade-in" data-aos-duration="1400" 
+            data-aos-delay="900"><i id="ico2" class="ri-instagram-line"></i></a>
+            <a href="#" data-aos="fade-in" data-aos-duration="1400" 
+            data-aos-delay="1200"><i id="ico3" class="ri-whatsapp-line"></i></a>
+        </div>
+
+    </header>  
+    
+    <section class="elite" id="servicios-elite">
+        <img class="elite-img" src="images/Partescomp.png" alt="">
+
+        <div class="elite-content container">
+            <h2>ELITE A TU SERVICIO</h2>
+            <p class="txt-p">
+                Conoce todos los servicios que podemos ofecerte
+            </p>
+
+            <div class="elite-group">
+
+                <div class="elite-1">
+                    <img src="imagenes/mujer.jpg" alt="">
+                    <h3>TRASPORTE ESCOLAR</h3>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Excepturi autem, consectetur nisi repudiandae error aliquid laboriosam 
+                        voluptatibus ab repellat, obcaecati distinctio quis nulla deserunt.                        
+                    </p>
+                </div>
+
+                <div class="elite-1">
+                    <img src="imagenes/mujer.jpg" alt="">
+                    <h3>TRANSLADO AEROPUERTO</h3>
+                    <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Excepturi autem, consectetur nisi repudiandae error aliquid laboriosam 
+                        voluptatibus ab repellat, obcaecati distinctio quis nulla deserunt.                       
+                    </p>
+                </div>
+                
+                <div class="elite-1">
+                    <img src="imagenes/mujer.jpg" alt="">
+                    <h3>SERVICIO A NIVEL NACIONAL</h3>
+                    <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Excepturi autem, consectetur nisi repudiandae error aliquid laboriosam 
+                        voluptatibus ab repellat, obcaecati distinctio quis nulla deserunt.                        
+                    </p>
+                </div>
+                                
+            </div>
+<!--            <a href="#" class="btn-1">Información</a>   -->
+        </div>
+    </section> 
+
+    <main class="services">
+
+        <div class="services-content container ">
+            <h2>TIPOS DE VEHICULOS</h2>
+                <div class="services-group">
+                    <div class="services-1">
+                        <img src="imagenes/buseta.jpg" alt="">
+                        <h3>BUS</h3>
+                    </div>
+
+                    <div class="services-1">
+                        <img src="imagenes/escolar.jpg" alt="">
+                        <h3>MINI VAN</h3>
+                    </div>
+
+                    <div class="services-1">
+                        <img src="imagenes/duster.jpg" alt="">
+                        <h3>CAMIONETA</h3>
+                    </div>
+                </div>
+
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus impedit dicta veritatis, 
+            aut numquam, explicabo quasi iste velit similique, nostrum dolor ab temporibus 
+            ipsam dolorum eius. Illo, vel aperiam?
+            </p>    
+<!--            <a href="#" class="btn-1">Información</a>  -->
+        </div>
+
+    </main>
+
+<!--    <section class="general">
+        
+        <div class="general-1">
+            <h2>voluptate aliquid</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Illum modi perferendis perspiciatis ipsa omnis voluptas est 
+                qui vitae voluptate aliquid eum exercitationem, pariatur doloribus corporis a? 
+                Magnam odio dolorum consequuntur?
+            </p>
+            <a href="#" class="btn-1">Información</a>  
+        </div>
+        <div class="general-2"></div>
+    </section>
+
+    <section class="general">
+
+        <div class="general-3"></div>
+        
+        <div class="general-1">
+            <h2>voluptate aliquid</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Illum modi perferendis perspiciatis ipsa omnis voluptas est 
+                qui vitae voluptate aliquid eum exercitationem, pariatur doloribus corporis a? 
+                Magnam odio dolorum consequuntur?
+            </p>
+            <a href="#" class="btn-1">Información</a>  
+        </div>
+    </section>
+
+    <section class="blog container">
+
+        <h2>Blog</h2>
+        <p>Illum modi perferendis perspiciatis ipsa omnis voluptas est</p>
+
+        <div class="blog-content">
+            <div class="blog-1">
+                <img src="images/blog1.jpg" alt="">
+                <h3>Blog 1</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Illum modi perferendis perspiciatis ipsa omnis voluptas est  
+                </p>
+            </div>
+
+            <div class="blog-1">
+                <img src="images/blog2.jpg" alt="">
+                <h3>Blog 2</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Illum modi perferendis perspiciatis ipsa omnis voluptas est  
+                </p>
+            </div>
+
+            <div class="blog-1">
+                <img src="images/blog3.jpg" alt="">
+                <h3>Blog 3</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Illum modi perferendis perspiciatis ipsa omnis voluptas est  
+                </p>
+            </div>
+        </div>
+        <a href="#" class="btn-1">Información</a>
+    </section>
+-->
+
+<footer>
+<div class="footer-container">
+        <div class="section1">
+            <h2>Transportes Elite SAS</h2>
+            <p>Viajes seguros <br> cumplimiento garantizado.</p>
+            <div class="footer-icons">
+            <a href="#"><i class="ri-facebook-fill"></i></a>
+            <a href="#"><i class="ri-instagram-fill"></i></a>
+            <a href="#"><i class="ri-whatsapp-fill"></i></a>
+            </div>
+        </div>
+        
+        <div class="section2">
+            <h2>Datos de Contacto</h2>
+            <p>📍 Cra. 66 a #11-26 B/Limonar Cali - Valle</p>
+            <p>📞 +57 321 758 7889</p>
+            <p>🏢 8:00 AM - 5:00 PM Lunes a Viernes</p>
+        </div>
+
+        <div class="section3">
+            <h2>Nuestra Ubicación</h2>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!
+                1d3982.806648487712!2d-76.5398194!3d3.3973124!2m3!1f0!2f0!3f0!3m2
+                !1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a151abf0e8a3%3A0x4c69338becf02462!2s
+                Cra.%2066a%20%2311-26%2C%20Comuna%2017%2C%20Cali%2C%20Valle%20del%20Cauca!5e0!3m2!1ses
+                !2sco!4v1743039155562!5m2!1ses!2sco" 
+                allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+</footer>
+
+    <script src="js/script.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            offset:1
+        });
+    </script>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+        const menuIcon = document.getElementById("menu-icon");
+        const navbar = document.querySelector(".navbar");
+
+        menuIcon.addEventListener("click", function() {
+            navbar.classList.toggle("active");
+        });
+
+        // Cierra el menú cuando se hace clic en un enlace
+        document.querySelectorAll(".navbar ul li a").forEach(link => {
+            link.addEventListener("click", function() {
+                navbar.classList.remove("active");
+            });
+        });
+    });
+    </script>
+</body>
+</html>
